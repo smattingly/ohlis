acc7a00b4c42a484ce2ec989e2da679c05796973
Steve Mattingly <mattingly@fastmail.com>
2026-08-19 22:07:49 -0400

    chore: troubleshoot Playwright CI WebKit

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,6 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "list",
-  expect: { timeout: 20_000 },
 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
