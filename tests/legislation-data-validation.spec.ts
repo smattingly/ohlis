cce1699e1e0073496bd5885252d1871ef2384047
Steve Mattingly <mattingly@fastmail.com>
2026-08-20 10:58:41 -0400

    chore: troubleshoot Playwright CI: test CSS class, not visibility

--- tests/legislation-data-validation.spec.ts
+++ tests/legislation-data-validation.spec.ts
@@ -4,24 +4,28 @@ test("Given incomplete data, When create is attempted, Then error is shown.", as
   page,
 }) => {
   const url = "legislation/new";
+  // Bootstrap uses the following CSS class to mark form inputs that fail data validation.
+  // Test for this rather than visibility of error messages, because some WebKit versions
+  // seemed to generate false positives due to weird rendering.
+  const invalidDataInput = "is-invalid";
 
   // Nothing entered
   await page.goto(url);
   await page.locator("#submit").click();
-  await expect(page.locator("#titleFeedback")).toBeVisible();
-  await expect(page.locator("#textFeedback")).toBeVisible();
+  await expect(page.locator("#title")).toContainClass(invalidDataInput);
+  await expect(page.locator("#text")).toContainClass(invalidDataInput);
 
   // Title only entered.
   await page.goto(url);
   await page.getByRole("textbox", { name: "Title:" }).fill("title");
   await page.locator("#submit").click();
-  await expect(page.locator("#titleFeedback")).not.toBeVisible();
-  await expect(page.locator("#textFeedback")).toBeVisible();
+  await expect(page.locator("#title")).not.toContainClass(invalidDataInput);
+  await expect(page.locator("#text")).toContainClass(invalidDataInput);
 
   // Text only entered.
   await page.goto(url);
   await page.getByRole("textbox", { name: "Text:" }).fill("test");
   await page.locator("#submit").click();
-  await expect(page.locator("#titleFeedback")).toBeVisible();
-  await expect(page.locator("#textFeedback")).not.toBeVisible();
+  await expect(page.locator("#title")).toContainClass(invalidDataInput);
+  await expect(page.locator("#text")).not.toContainClass(invalidDataInput);
 });
--- tests/legislators-data-validation.spec.ts
+++ tests/legislators-data-validation.spec.ts
@@ -5,61 +5,66 @@ test("Given incomplete data, When create is attempted, Then error is shown.", as
 }) => {
   const url = "legislators/new";
 
+  // Bootstrap uses the following CSS class to mark form inputs that fail data validation.
+  // Test for this rather than visibility of error messages, because some WebKit versions
+  // seemed to generate false positives due to weird rendering.
+  const invalidDataInput = "is-invalid";
+
   // Nothing entered
   await page.goto(url);
   await page.locator("#submit").click();
-  await expect(page.locator("#firstNameFeedback")).toBeVisible();
-  await expect(page.locator("#lastNameFeedback")).toBeVisible();
-  await expect(page.locator("#hometownFeedback")).toBeVisible();
+  await expect(page.locator("#firstName")).toContainClass(invalidDataInput);
+  await expect(page.locator("#lastName")).toContainClass(invalidDataInput);
+  await expect(page.locator("#hometown")).toContainClass(invalidDataInput);
 
   // First name only entered.
   await page.goto(url);
   await page.getByRole("textbox", { name: "First Name:" }).fill("first");
   await page.locator("#submit").click();
-  await expect(page.locator("#firstNameFeedback")).not.toBeVisible();
-  await expect(page.locator("#lastNameFeedback")).toBeVisible();
-  await expect(page.locator("#hometownFeedback")).toBeVisible();
+  await expect(page.locator("#firstName")).not.toContainClass(invalidDataInput);
+  await expect(page.locator("#lastName")).toContainClass(invalidDataInput);
+  await expect(page.locator("#hometown")).toContainClass(invalidDataInput);
 
   // Last name only entered.
   await page.goto(url);
   await page.getByRole("textbox", { name: "Last Name:" }).fill("last");
   await page.locator("#submit").click();
-  await expect(page.locator("#firstNameFeedback")).toBeVisible();
-  await expect(page.locator("#lastNameFeedback")).not.toBeVisible();
-  await expect(page.locator("#hometownFeedback")).toBeVisible();
+  await expect(page.locator("#firstName")).toContainClass(invalidDataInput);
+  await expect(page.locator("#lastName")).not.toContainClass(invalidDataInput);
+  await expect(page.locator("#hometown")).toContainClass(invalidDataInput);
 
   // Hometown only entered.
   await page.goto(url);
   await page.getByRole("textbox", { name: "Hometown:" }).fill("hometown");
   await page.locator("#submit").click();
-  await expect(page.locator("#firstNameFeedback")).toBeVisible();
-  await expect(page.locator("#lastNameFeedback")).toBeVisible();
-  await expect(page.locator("#hometownFeedback")).not.toBeVisible();
+  await expect(page.locator("#firstName")).toContainClass(invalidDataInput);
+  await expect(page.locator("#lastName")).toContainClass(invalidDataInput);
+  await expect(page.locator("#hometown")).not.toContainClass(invalidDataInput);
 
   // First name and Last name entered.
   await page.goto(url);
   await page.getByRole("textbox", { name: "First Name:" }).fill("first");
   await page.getByRole("textbox", { name: "Last Name:" }).fill("last");
   await page.locator("#submit").click();
-  await expect(page.locator("#firstNameFeedback")).not.toBeVisible();
-  await expect(page.locator("#lastNameFeedback")).not.toBeVisible();
-  await expect(page.locator("#hometownFeedback")).toBeVisible();
+  await expect(page.locator("#firstName")).not.toContainClass(invalidDataInput);
+  await expect(page.locator("#lastName")).not.toContainClass(invalidDataInput);
+  await expect(page.locator("#hometown")).toContainClass(invalidDataInput);
 
   // First name and Hometown entered.
   await page.goto(url);
   await page.getByRole("textbox", { name: "First Name:" }).fill("first");
   await page.getByRole("textbox", { name: "Hometown:" }).fill("hometown");
   await page.locator("#submit").click();
-  await expect(page.locator("#firstNameFeedback")).not.toBeVisible();
-  await expect(page.locator("#lastNameFeedback")).toBeVisible();
-  await expect(page.locator("#hometownFeedback")).not.toBeVisible();
+  await expect(page.locator("#firstName")).not.toContainClass(invalidDataInput);
+  await expect(page.locator("#lastName")).toContainClass(invalidDataInput);
+  await expect(page.locator("#hometown")).not.toContainClass(invalidDataInput);
 
   // Last name and Hometown entered.
   await page.goto(url);
   await page.getByRole("textbox", { name: "Last Name:" }).fill("last");
   await page.getByRole("textbox", { name: "Hometown:" }).fill("hometown");
   await page.locator("#submit").click();
-  await expect(page.locator("#firstNameFeedback")).toBeVisible();
-  await expect(page.locator("#lastNameFeedback")).not.toBeVisible();
-  await expect(page.locator("#hometownFeedback")).not.toBeVisible();
+  await expect(page.locator("#firstName")).toContainClass(invalidDataInput);
+  await expect(page.locator("#lastName")).not.toContainClass(invalidDataInput);
+  await expect(page.locator("#hometown")).not.toContainClass(invalidDataInput);
 });
